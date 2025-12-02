d49b65baf340d45429c88d5bff903b191874980a
mitkalinsky <mit.kalinsky@gmail.com>
2025-12-02 20:46:48 +0300

    added the concept and done with adaptivity for diferent gadgets

--- uni_python/uni_python_project1.py
+++ uni_python/uni_python_project1.py
@@ -1,1 +1,9 @@
-print ("Hello, World!")
+years = input().split()         # читаем года как строки, разделённые пробелами
+limit = int(input())            # читаем число, с которым сравниваем
+years = list(map(int, years))
+
+for y in years:
+    year = int(y)               # переводим каждый год в число
+    if year > limit:            # проверяем условие
+        print(year)
+    
